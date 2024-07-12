3df3c0a7f75dd92e36b67716b7f8648146433259
Aksam <aksam.iftikhar@gmail.com>
2024-07-12 09:10:07 +0500

    modified:   src/main.cpp (modified by user2)

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,25 @@
 #include "world.h"
 #include "robot.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   // You should customize this data structure and the World constructor to
   // match your chosen data structure.
   char myMap[7][7] = {
-    {'1', '1', '1', '1', '1', '1', '1'},
-    {'1', '0', '0', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '0', '0', '1'},
-    {'1', '0', '1', '1', '1', '1', '1'},
-    {'1', '0', '0', '0', '0', '0', '1'},
-    {'1', '0', '1', '0', '0', '0', '1'},
-    {'1', '1', '1', '1', '1', '1', '1'},
+      {'1', '1', '1', '1', '1', '1', '1'},
+      {'1', '0', '0', '0', '0', '0', '1'},
+      {'1', '0', '0', '0', '0', '0', '1'},
+      {'1', '0', '1', '1', '1', '1', '1'},
+      {'1', '0', '0', '0', '0', '0', '1'},
+      {'1', '0', '1', '0', '0', '0', '1'},
+      {'1', '1', '1', '1', '1', '1', '1'},
   };
-  
-  //World w;      // uninitialized (empty) world
+
+  // comment in main from second user
+
+  // World w;      // uninitialized (empty) world
   World w(myMap); // world initialized to myMap
-      
+
   // place a couple of robots in world, and run the world
   position p = {1, 1}; // position is defined in robot.h
   Robot r('+', p, 4, move_dir::RIGHT);
@@ -31,19 +34,19 @@ int main(int argc, char** argv) {
   r2.setDirectionChangeMethod(direction_changer::CC_DIRECTION);
   r2.setRobotDirection(move_dir::DOWN); // move_dir is defined in robot.h
   w.addRobot(2, 3, &r2);
-  
+
   w.display();
   std::cout << "Initial world map with robots is shown above." << std::endl;
   std::cout << "Press Enter to start moving robots..." << std::endl;
   getchar();
-  
+
   std::cout << "going to start the movement in 3 seconds ..." << std::endl;
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  std::cout << "going to start the movement in 2 seconds ..."<< std::endl;
+  std::cout << "going to start the movement in 2 seconds ..." << std::endl;
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  std::cout << "going to start the movement in 1 seconds ..."<< std::endl;
+  std::cout << "going to start the movement in 1 seconds ..." << std::endl;
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  std::cout << "movement started "<< std::endl;
+  std::cout << "movement started " << std::endl;
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
   // start robots movement
